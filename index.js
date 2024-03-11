1d59cc62d6dc21ed607b8a222d164096fc6c074f
Julie Poulain <juliempoulain@gmail.com>
2024-03-11 15:44:05 -0700

    added functions

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function (parameter1, parameter2) {
+    return parameter1/parameter2
+}
+
+const square = parameter => parameter*parameter
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
